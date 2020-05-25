90588e28268229eaf6440bda14e1555f84cbdec3
germanlop05 <00108120@uca.edu.sv>
2020-05-25 13:23:46 -0600

    commit

--- laboratorio 4/ejercicio4.cpp
+++ laboratorio 4/ejercicio4.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    
+ string palabra;
+
+cout<<"ingrese palabra: ";
+cin>>palabra;
+
+int longitud= palabra.length();
+
+cout<<palabra<<" tiene una longitud de "<<longitud<<" caracteres"<<endl;
+
+if (longitud>10){
+cout<<palabra<< " tiene mas de 10 caracteres, "<<" tiene "<<longitud<<endl;
+}
+else if (longitud<10){
+    cout<<palabra<<"  tiene menos de 10 caracteres, "<< " tiene "<<longitud<<endl;
+}
+else if (longitud==10){
+    cout<<palabra<<" es igual a 10 caracteres, "<< " tiene " <<longitud<<endl;
+}
+if (longitud%2==0){
+    cout<<palabra<<" tiene longitud par"<<endl;
+} else{
+    cout<<palabra<<" tiene longitud impar"<<endl;
+}
+
+return 0;
 }
