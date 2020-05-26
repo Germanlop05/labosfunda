9b64a563d5701dcc7e9aefa86abaac32660d86d3
germanlop05 <00108120@uca.edu.sv>
2020-05-25 21:14:53 -0600

    commit

--- laboratorio 4/ejercicio5.cpp
+++ laboratorio 4/ejercicio5.cpp
@@ -6,10 +6,18 @@ using namespace std;
 int main()
 {
     string palabra;
+    
+cout<<"ingrese la palabra:";
+cin>>palabra;
 
+int longitud= palabra.length();
 
-
-   
+if(palabra[0]==palabra[longitud-1]){
+cout<<"sus letras son iguales "<<endl;
+}
+else {
+cout<<"sus letras no son iguales"<<endl;
+}
 
 return 0;
 }
